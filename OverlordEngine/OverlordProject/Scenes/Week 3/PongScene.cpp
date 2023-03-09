fd27dcb0eb105d2749da85bbcd56cca47f45aba1
LuckyLuke00 <lucas.kinoo5@gmail.com>
2023-03-09 19:38:43 +0100

    Implement GP2_W3X_ASSIGNMENT_1

--- OverlordEngine/OverlordProject/Scenes/Week 3/PongScene.cpp
+++ OverlordEngine/OverlordProject/Scenes/Week 3/PongScene.cpp
@@ -11,8 +11,8 @@ PongScene::PongScene() :
 void PongScene::Initialize()
 {
 	//m_SceneContext.settings.showInfoOverlay = true;
-	//m_SceneContext.settings.drawPhysXDebug = true;
-	m_SceneContext.settings.drawGrid = true;
+	m_SceneContext.settings.drawPhysXDebug = false;
+	m_SceneContext.settings.drawGrid = false;
 	m_SceneContext.settings.enableOnGUI = true;
 	m_SceneContext.settings.clearColor = XMFLOAT4{ Colors::Black };
 
@@ -32,6 +32,8 @@ void PongScene::Update()
 	{
 		ResetScene();
 	}
+
+	HandleInput();
 }
 
 void PongScene::Draw()
@@ -58,26 +60,26 @@ void PongScene::CreateCamera()
 
 void PongScene::CreatePaddles(const PxMaterial* material)
 {
-	constexpr float paddleWidth{ 4.f };
-
 	// Left Paddle
-	m_pLeftPaddle = new CubePrefab(1, 1, paddleWidth, XMFLOAT4(Colors::White));
+	m_pLeftPaddle = new CubePrefab(1, 1, m_PaddleWidth, XMFLOAT4(Colors::White));
 	m_pLeftPaddle->GetTransform()->Translate(-15, 0, 0);
+	m_LeftPaddlePosition = { -15, 0, 0 };
 	AddChild(m_pLeftPaddle);
 
 	// Rigid body component
 	auto pRigidBody{ m_pLeftPaddle->AddComponent(new RigidBodyComponent(false)) };
-	pRigidBody->AddCollider(PxBoxGeometry{ .5f, .5f, paddleWidth * .5f }, *material);
+	pRigidBody->AddCollider(PxBoxGeometry{ .5f, .5f, m_PaddleWidth * .5f }, *material);
 	pRigidBody->SetKinematic(true);
 
 	// Right Paddle
-	m_pRightPaddle = new CubePrefab(1, 1, paddleWidth, XMFLOAT4(Colors::White));
+	m_pRightPaddle = new CubePrefab(1, 1, m_PaddleWidth, XMFLOAT4(Colors::White));
 	m_pRightPaddle->GetTransform()->Translate(15, 0, 0);
+	m_RightPaddlePosition = { 15, 0, 0 };
 	AddChild(m_pRightPaddle);
 
 	// Rigid body component
 	pRigidBody = m_pRightPaddle->AddComponent(new RigidBodyComponent(false));
-	pRigidBody->AddCollider(PxBoxGeometry{ .5f, .5f, paddleWidth * .5f }, *material);
+	pRigidBody->AddCollider(PxBoxGeometry{ .5f, .5f, m_PaddleWidth * .5f }, *material);
 	pRigidBody->SetKinematic(true);
 }
 
@@ -120,7 +122,7 @@ void PongScene::CreateWalls(const PxMaterial* material)
 {
 	// Top Wall
 	auto pLeftWall{ new CubePrefab(38, 1, 1, XMFLOAT4(Colors::Red)) };
-	pLeftWall->GetTransform()->Translate(0, 0, 11);
+	pLeftWall->GetTransform()->Translate(0, 0, m_WallOffset);
 	AddChild(pLeftWall);
 
 	// Rigid body component
@@ -130,7 +132,7 @@ void PongScene::CreateWalls(const PxMaterial* material)
 
 	// Right Wall
 	auto pRightWall{ new CubePrefab(38, 1, 1, XMFLOAT4(Colors::Red)) };
-	pRightWall->GetTransform()->Translate(0, 0, -11);
+	pRightWall->GetTransform()->Translate(0, 0, -m_WallOffset);
 	AddChild(pRightWall);
 
 	// Rigid body component
@@ -152,9 +154,60 @@ void PongScene::ResetScene()
 {
 	// Translate the ball to the middle of the screen
 	m_pBall->GetTransform()->Translate(0, .5f, 0);
-	m_pBall->GetComponent<RigidBodyComponent>()->AddForce({ 5, 0, 5 }, PxForceMode::eIMPULSE);
 
 	m_ResetScene = false;
+	m_Paused = true;
+}
+
+void PongScene::HandleInput()
+{
+	if (m_Paused && InputManager::IsKeyboardKey(InputState::pressed, VK_SPACE))
+	{
+		m_pBall->GetComponent<RigidBodyComponent>()->AddForce({ m_BallSpeed, 0, m_BallSpeed }, PxForceMode::eIMPULSE);
+		m_Paused = false;
+	}
+
+	// TODO: Find a way that collision between the paddles and walls is handled automatically
+	const bool rightPaddleTopCollision{ m_RightPaddlePosition.z + m_PaddleWidth * .5f >= m_WallOffset };
+	const bool rightPaddleBottomCollision{ m_RightPaddlePosition.z - m_PaddleWidth * .5f <= -m_WallOffset };
+	const bool leftPaddleTopCollision{ m_LeftPaddlePosition.z + m_PaddleWidth * .5f >= m_WallOffset };
+	const bool leftPaddleBottomCollision{ m_LeftPaddlePosition.z - m_PaddleWidth * .5f <= -m_WallOffset };
+
+	if (!rightPaddleTopCollision && InputManager::IsKeyboardKey(InputState::down, VK_UP))
+	{
+		// Get the kinematic
+		auto pRigidBody{ m_pRightPaddle->GetComponent<RigidBodyComponent>() };
+		m_RightPaddlePosition.z += m_PaddleSpeed * m_SceneContext.pGameTime->GetElapsed();
+
+		pRigidBody->GetPxRigidActor()->is<PxRigidDynamic>()->setKinematicTarget(PxTransform{ m_RightPaddlePosition });
+	}
+
+	if (!rightPaddleBottomCollision && InputManager::IsKeyboardKey(InputState::down, VK_DOWN))
+	{
+		// Get the kinematic
+		auto pRigidBody{ m_pRightPaddle->GetComponent<RigidBodyComponent>() };
+		m_RightPaddlePosition.z -= m_PaddleSpeed * m_SceneContext.pGameTime->GetElapsed();
+
+		pRigidBody->GetPxRigidActor()->is<PxRigidDynamic>()->setKinematicTarget(PxTransform{ m_RightPaddlePosition });
+	}
+
+	if (!leftPaddleTopCollision && InputManager::IsKeyboardKey(InputState::down, 'Q'))
+	{
+		// Get the kinematic
+		auto pRigidBody{ m_pLeftPaddle->GetComponent<RigidBodyComponent>() };
+		m_LeftPaddlePosition.z += m_PaddleSpeed * m_SceneContext.pGameTime->GetElapsed();
+
+		pRigidBody->GetPxRigidActor()->is<PxRigidDynamic>()->setKinematicTarget(PxTransform{ m_LeftPaddlePosition });
+	}
+
+	if (!leftPaddleBottomCollision && InputManager::IsKeyboardKey(InputState::down, 'A'))
+	{
+		// Get the kinematic
+		auto pRigidBody{ m_pLeftPaddle->GetComponent<RigidBodyComponent>() };
+		m_LeftPaddlePosition.z -= m_PaddleSpeed * m_SceneContext.pGameTime->GetElapsed();
+
+		pRigidBody->GetPxRigidActor()->is<PxRigidDynamic>()->setKinematicTarget(PxTransform{ m_LeftPaddlePosition });
+	}
 }
 
 void PongScene::CreateBall(const PxMaterial* material)
@@ -172,6 +225,4 @@ void PongScene::CreateBall(const PxMaterial* material)
 	auto pJoint{ PxD6JointCreate(pPhysX, nullptr, PxTransform{ PxIdentity }, pRigidBody->GetPxRigidActor(), PxTransform{ PxIdentity }) };
 	pJoint->setMotion(PxD6Axis::eX, PxD6Motion::eFREE);
 	pJoint->setMotion(PxD6Axis::eZ, PxD6Motion::eFREE);
-
-	m_pBall->GetComponent<RigidBodyComponent>()->AddForce({ 5, 0, 5 }, PxForceMode::eIMPULSE);
 }
--- OverlordEngine/OverlordProject/Scenes/Week 3/PongScene.h
+++ OverlordEngine/OverlordProject/Scenes/Week 3/PongScene.h
@@ -18,16 +18,26 @@ protected:
 	void OnGUI() override;
 
 private:
+	bool m_Paused{ true };
 	bool m_ResetScene{ false };
+
+	const float m_BallSpeed{ 10.f };
+	const float m_PaddleSpeed{ 10.f };
+	const float m_PaddleWidth{ 4.f };
+	const float m_WallOffset{ 11.f };
 
 	FixedCamera* m_pCamera{ nullptr };
 
 	GameObject* m_pBall{ nullptr };
 
 	GameObject* m_pLeftPaddle{ nullptr };
+	PxVec3 m_LeftPaddlePosition{ .0f, .0f, .0f };
+
 	GameObject* m_pRightPaddle{ nullptr };
+	PxVec3 m_RightPaddlePosition{ .0f, .0f, .0f };
 
 	GameObject* m_pLeftTrigger{ nullptr };
+
 	GameObject* m_pRightTrigger{ nullptr };
 
 	void CreateBall(const PxMaterial* material);
@@ -39,4 +49,6 @@ private:
 	void OnTrigger(GameObject* pTriggerObject, GameObject* pOtherObject, PxTriggerAction action);
 
 	void ResetScene();
+
+	void HandleInput();
 };
