a1a82191d5db85447d1c84450142929dad701254
LuckyLuke00 <lucas.kinoo5@gmail.com>
2023-03-29 11:08:45 +0200

    Implement HardwareSkinningScene

--- OverlordEngine/OverlordEngine/Misc/ModelAnimator.cpp
+++ OverlordEngine/OverlordEngine/Misc/ModelAnimator.cpp
@@ -7,96 +7,161 @@ ModelAnimator::ModelAnimator(MeshFilter* pMeshFilter) :
 	SetAnimation(0);
 }
 
-void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
+void ModelAnimator::Update(const SceneContext& sceneContext)
 {
-	TODO_W6_();
-
 	//We only update the transforms if the animation is running and the clip is set
 	if (m_IsPlaying && m_ClipSet)
 	{
 		//1.
 		//Calculate the passedTicks (see the lab document)
-		//auto passedTicks = ...
+		auto passedTicks{ sceneContext.pGameTime->GetElapsed() * m_CurrentClip.ticksPerSecond * m_AnimationSpeed };
 		//Make sure that the passedTicks stay between the m_CurrentClip.Duration bounds (fmod)
+		passedTicks = fmod(passedTicks, m_CurrentClip.duration);
 
 		//2.
 		//IF m_Reversed is true
-		//	Subtract passedTicks from m_TickCount
-		//	If m_TickCount is smaller than zero, add m_CurrentClip.Duration to m_TickCount
+		if (m_Reversed)
+		{
+			//	Subtract passedTicks from m_TickCount
+			m_TickCount -= passedTicks;
+
+			//	If m_TickCount is smaller than zero, add m_CurrentClip.Duration to m_TickCount
+			if (m_TickCount < 0)
+			{
+				m_TickCount += m_CurrentClip.duration;
+			}
+		}
 		//ELSE
-		//	Add passedTicks to m_TickCount
-		//	if m_TickCount is bigger than the clip duration, subtract the duration from m_TickCount
+		else
+		{
+			//	Add passedTicks to m_TickCount
+			m_TickCount += passedTicks;
+
+			//	if m_TickCount is bigger than the clip duration, subtract the duration from m_TickCount
+			if (m_TickCount > m_CurrentClip.duration)
+			{
+				m_TickCount -= m_CurrentClip.duration;
+			}
+		}
 
 		//3.
 		//Find the enclosing keys
 		AnimationKey keyA, keyB;
 		//Iterate all the keys of the clip and find the following keys:
-		//keyA > Closest Key with Tick before/smaller than m_TickCount
-		//keyB > Closest Key with Tick after/bigger than m_TickCount
+		for (const auto& key : m_CurrentClip.keys)
+		{
+			//keyA > Closest Key with Tick before/smaller than m_TickCount
+			if (key.tick < m_TickCount)
+			{
+				keyA = key;
+			}
+			//keyB > Closest Key with Tick after/bigger than m_TickCount
+			else if (key.tick > m_TickCount)
+			{
+				keyB = key;
+				break;
+			}
+		}
 
 		//4.
 		//Interpolate between keys
 		//Figure out the BlendFactor (See lab document)
+		const float blendFactor{ (m_TickCount - keyA.tick) / (keyB.tick - keyA.tick) };
+
 		//Clear the m_Transforms vector
+		m_Transforms.clear();
+
 		//FOR every boneTransform in a key (So for every bone)
-		//	Retrieve the transform from keyA (transformA)
-		//	auto transformA = ...
-		// 	Retrieve the transform from keyB (transformB)
-		//	auto transformB = ...
-		//	Decompose both transforms
-		//	Lerp between all the transformations (Position, Scale, Rotation)
-		//	Compose a transformation matrix with the lerp-results
-		//	Add the resulting matrix to the m_Transforms vector
+		for (size_t i{ 0 }; i < keyA.boneTransforms.size(); ++i)
+		{
+			//	Retrieve the transform from keyA (transformA)
+			const auto transformA{ keyA.boneTransforms[i] };
+			// 	Retrieve the transform from keyB (transformB)
+			const auto transformB{ keyB.boneTransforms[i] };
+
+			//	Decompose both transforms
+			XMVECTOR scaleA, rotA, transA;
+			XMMatrixDecompose(&scaleA, &rotA, &transA, XMLoadFloat4x4(&transformA));
+
+			XMVECTOR scaleB, rotB, transB;
+			XMMatrixDecompose(&scaleB, &rotB, &transB, XMLoadFloat4x4(&transformB));
+			//	Lerp between all the transformations (Position, Scale, Rotation)
+			const auto lerpScale{ XMVectorLerp(scaleA, scaleB, blendFactor) };
+			const auto lerpRot{ XMQuaternionSlerp(rotA, rotB, blendFactor) };
+			const auto lerpPos{ XMVectorLerp(transA, transB, blendFactor) };
+
+			//	Compose a transformation matrix with the lerp-results
+			XMMATRIX transform{ XMMatrixTransformation({}, {}, lerpScale, {}, lerpRot, lerpPos) };
+			//	Add the resulting matrix to the m_Transforms vector
+			XMFLOAT4X4 finalTransform;
+			XMStoreFloat4x4(&finalTransform, transform);
+			m_Transforms.push_back(finalTransform);
+		}
 	}
 }
 
-void ModelAnimator::SetAnimation(const std::wstring& /*clipName*/)
+void ModelAnimator::SetAnimation(const std::wstring& clipName)
 {
-	TODO_W6_()
-		//Set m_ClipSet to false
-		//Iterate the m_AnimationClips vector and search for an AnimationClip with the given name (clipName)
-		//If found,
-		//	Call SetAnimation(Animation Clip) with the found clip
-		//Else
-		//	Call Reset
-		//	Log a warning with an appropriate message
+	//Set m_ClipSet to false
+	m_ClipSet = false;
+	//Iterate the m_AnimationClips vector and search for an AnimationClip with the given name (clipName)
+	for (UINT i{ 0 }; i < GetClipCount(); ++i)
+	{
+		if (GetClip(i).name == clipName)
+		{
+			SetAnimation(GetClip(i));
+			return;
+		}
+
+		Reset(false);
+		Logger::LogWarning(L"ModelAnimator::SetAnimation(UINT) > clipNumber is out of range!");
+	}
 }
 
-void ModelAnimator::SetAnimation(UINT /*clipNumber*/)
+void ModelAnimator::SetAnimation(UINT clipNumber)
 {
-	TODO_W6_()
-		//Set m_ClipSet to false
-		//Check if clipNumber is smaller than the actual m_AnimationClips vector size
-		//If not,
-			//	Call Reset
-			//	Log a warning with an appropriate message
-			//	return
-		//else
-			//	Retrieve the AnimationClip from the m_AnimationClips vector based on the given clipNumber
-			//	Call SetAnimation(AnimationClip clip)
+	m_ClipSet = false;
+	if (clipNumber < GetClipCount())
+	{
+		SetAnimation(GetClip(clipNumber));
+	}
+	else
+	{
+		Reset(false);
+		Logger::LogWarning(L"ModelAnimator::SetAnimation(UINT) > clipNumber is out of range!");
+	}
 }
 
-void ModelAnimator::SetAnimation(const AnimationClip& /*clip*/)
+void ModelAnimator::SetAnimation(const AnimationClip& clip)
 {
-	TODO_W6_()
-		//Set m_ClipSet to true
-		//Set m_CurrentClip
-
-		//Call Reset(false)
+	m_ClipSet = true;
+	m_CurrentClip = clip;
+	Reset(false);
 }
 
-void ModelAnimator::Reset(bool /*pause*/)
+void ModelAnimator::Reset(bool pause)
 {
-	TODO_W6_()
-		//If pause is true, set m_IsPlaying to false
+	//If pause is true, set m_IsPlaying to false
+	if (pause) m_IsPlaying = false;
 
-		//Set m_TickCount to zero
-		//Set m_AnimationSpeed to 1.0f
+	//Set m_TickCount to zero
+	m_TickCount = .0f;
+	//Set m_AnimationSpeed to 1.0f
+	m_AnimationSpeed = 1.f;
 
-		//If m_ClipSet is true
+	//If m_ClipSet is true
+	if (m_ClipSet)
+	{
 		//	Retrieve the BoneTransform from the first Key from the current clip (m_CurrentClip)
 		//	Refill the m_Transforms vector with the new BoneTransforms (have a look at vector::assign)
-		//Else
+		const auto boneTransform{ m_CurrentClip.keys[0].boneTransforms };
+		m_Transforms.assign(boneTransform.begin(), boneTransform.end());
+	}
+	else
+	{
 		//	Create an IdentityMatrix
 		//	Refill the m_Transforms vector with this IdenityMatrix (Amount = BoneCount) (have a look at vector::assign)
+		const XMFLOAT4X4 identity{ XMFLOAT4X4{} };
+		m_Transforms.assign(m_pMeshFilter->m_BoneCount, identity);
+	}
 }
--- OverlordEngine/OverlordProject/Materials/DiffuseMaterial_Skinned.cpp
+++ OverlordEngine/OverlordProject/Materials/DiffuseMaterial_Skinned.cpp
@@ -16,10 +16,14 @@ void DiffuseMaterial_Skinned::InitializeEffectVariables()
 {
 }
 
-void DiffuseMaterial_Skinned::OnUpdateModelVariables(const SceneContext&, const ModelComponent* /*pModel*/) const
+void DiffuseMaterial_Skinned::OnUpdateModelVariables(const SceneContext&, const ModelComponent* pModel) const
 {
 	//Retrieve The Animator from the ModelComponent
+	const auto pAnimator{ pModel->GetAnimator() };
 	//Make sure the animator is not NULL (ASSERT_NULL_)
+	ASSERT_NULL_(pAnimator);
 	//Retrieve the BoneTransforms from the Animator
+	const auto& boneTransforms{ pAnimator->GetBoneTransforms() };
 	//Set the 'gBones' variable of the effect (MatrixArray) > BoneTransforms
+	SetVariable_MatrixArray(L"gBones", &boneTransforms.begin()->m[0][0], static_cast<UINT>(boneTransforms.size()));
 }
