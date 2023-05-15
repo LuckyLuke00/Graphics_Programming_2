0674173aea037797c3a938861883bd3860c7e3a5
LuckyLuke00 <lucas.kinoo5@gmail.com>
2023-05-15 09:44:13 +0200

    Enable Milestone 2 define

--- OverlordEngine/OverlordProject/MainGame.cpp
+++ OverlordEngine/OverlordProject/MainGame.cpp
@@ -9,11 +9,11 @@
 //#define W7
 //#define W8
 //#define W9
-#define W10
+//#define W10
 
 /*MILESTONE Content*/
 //#define MILESTONE_1
-// #define MILESTONE_2
+#define MILESTONE_2
 
 #pragma region Lab/Milestone Includes
 #ifdef W3
@@ -85,9 +85,9 @@ void MainGame::OnGamePreparing(GameContext& gameContext)
 	//gameContext.windowWidth=... (default is 1280)
 	//gameContext.windowHeight=... (default is 720)
 
-	gameContext.windowTitle = L"GP2 - Milestone 1 (2023) | (2DAE15) Kinoo Lucas";
-	//gameContext.windowTitle = L"GP2 - Milestone 2 (2023) | (2DAE13) Doe John";
-	//gameContext.windowTitle = L"GP2 - Exam Project (2023) | (2DAE13) Doe John";
+	//gameContext.windowTitle = L"GP2 - Milestone 1 (2023) | (2DAE15) Kinoo Lucas";
+	gameContext.windowTitle = L"GP2 - Milestone 2 (2023) | (2DAE15) Kinoo Lucas";
+	//gameContext.windowTitle = L"GP2 - Exam Project (2023) | (2DAE15) Kinoo Lucas";
 }
 
 void MainGame::Initialize()
